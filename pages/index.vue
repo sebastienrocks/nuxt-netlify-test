ddae36c50bfbb700e66e080d2dacc712567d4310
Sebastien <sebastien@macs-MacBook.local>
2018-07-12 13:22:46 +0100

    transformed to portfolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,18 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        test-project
+        Sebastien.rocks
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project TEST
+        New portfolio coming soon
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
+export default {}
 </script>
 
 <style>
@@ -52,13 +35,9 @@ export default {
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 36px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
